df40428b3711457ba2b664f398d9fa89e0e2aa1e
Leman Mirzeyeva <lmnmrzyv313@gmail.com>
2025-02-11 12:04:13 +0400

    Part 7 and Part 8

---
+++ Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_BecomeADriverComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CarBook.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _BecomeADriverComponentPartial:ViewComponent
+    {     
+        public IViewComponentResult Invoke()
+        {           
+            return View();
+        }
+    }
+}
